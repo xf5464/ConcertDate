e7a35c77fd95a9609e524fa6a01804be0cacf98d
xf5464 <xf5464@foxmail.com>
2026-09-07 18:44:45 +0800

    fix: publish hashed PWA assets directly

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE = 'concertdate-pwa-ba816ddb';
+const STATIC_FILES = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './assets/app-a14c9f11.js',
+  './assets/styles-91563071.css',
+  './assets/apple-touch-icon-ba816ddb.png',
+  './assets/icon-192-ba816ddb.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.pathname.endsWith('/data/concerts.json')) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
